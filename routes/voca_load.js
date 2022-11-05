0d1f86e3caef24eca5ceb2e35243321a9ea4985d
billlee <billlee2656@gmail.com>
2022-11-05 19:31:56 +0900

    deleteTTS

--- routes/voca_load.js
+++ routes/voca_load.js
@@ -4,17 +4,16 @@ const router = express.Router();
 const mysql = require("mysql");
 const db = mysql.createConnection(require("../lib/config").user);
 db.connect();
-const pollyObj = require("../lib/config").polly;
 const mymodule = require("../lib/mymodule");
-const AWS = require("aws-sdk");
-const Stream = require("stream");
-const Speaker = require("speaker");
+// const pollyObj = require("../lib/config").polly;
+// const AWS = require("aws-sdk");
+// const Stream = require("stream");
+// const Speaker = require("speaker");
+// const Polly = new AWS.Polly(pollyObj);
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const Polly = new AWS.Polly(pollyObj);
 
 router.post("/voca_load", (req, res) => {
   const post = req.body;
@@ -67,33 +66,34 @@ router.post("/voca_study", (req, res) => {
   );
 });
 
-router.post("/voca_audio", (req, res) => {
-  const post = req.body;
-  const Player = new Speaker({
-    channels: 1,
-    bitDepth: 16,
-    sampleRate: 16000,
-  });
-  const params = {
-    Text: post.body,
-    OutputFormat: "pcm",
-    VoiceId: "Matthew",
-  };
-  Polly.synthesizeSpeech(params, (err, data) => {
-    if (err) {
-      console.log(err.code);
-    } else if (data) {
-      if (data.AudioStream instanceof Buffer) {
-        // Initiate the source
-        let bufferStream = new Stream.PassThrough();
-        // convert AudioStream into a readable stream
-        bufferStream.end(data.AudioStream);
-        // Pipe into Player
-        bufferStream.pipe(Player);
-      }
-    }
-  });
-});
+// tts서비스 잠정중단
+// router.post("/voca_audio", (req, res) => {
+//   const post = req.body;
+//   const Player = new Speaker({
+//     channels: 1,
+//     bitDepth: 16,
+//     sampleRate: 16000,
+//   });
+//   const params = {
+//     Text: post.body,
+//     OutputFormat: "pcm",
+//     VoiceId: "Matthew",
+//   };
+//   Polly.synthesizeSpeech(params, (err, data) => {
+//     if (err) {
+//       console.log(err.code);
+//     } else if (data) {
+//       if (data.AudioStream instanceof Buffer) {
+//         // Initiate the source
+//         let bufferStream = new Stream.PassThrough();
+//         // convert AudioStream into a readable stream
+//         bufferStream.end(data.AudioStream);
+//         // Pipe into Player
+//         bufferStream.pipe(Player);
+//       }
+//     }
+//   });
+// });
 
 router.post("/search", (req, res) => {
   const post = req.body;
